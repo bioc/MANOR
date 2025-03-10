10c6d0594362f91c064fb26431993913f21f8969
Pierre Neuvial <pierre.neuvial@math.cnrs.fr>
2025-03-10 23:10:34 +0100

    try to fix long standing WARNING in error handling

--- src/exememo.c
+++ src/exememo.c
@@ -1,18 +1,18 @@
 /*\
-    exememo.c
+ exememo.c
 
-    Specific routines for operating system memory allocation
+ Specific routines for operating system memory allocation
 
-    Jan 1998
+ Jan 1998
 
-    Van Mo DANG
-    Universite de Technologie de Compiegne
-    URA CNRS 817
+ Van Mo DANG
+ Universite de Technologie de Compiegne
+ URA CNRS 817
 
-    Vers-mod  Date         Description
+ Vers-mod  Date         Description
 
-    1.05-a    26-JAN-1998  Creation
-\*/
+ 1.05-a    26-JAN-1998  Creation
+ \*/
 
 
 #include <stdlib.h>  /* calloc, free, size_t */
@@ -22,35 +22,35 @@
 /* ------------------------------------------------------------------- */
 void* GenAlloc
 (
- size_t       nelem,        /* I : number of elements to allocate */
- size_t       elsize,       /* I : size in bytes of each element */
- int          doexit,       /* I : 1 if failure exits, 0 if only return NULL */
- const char*  where,        /* I : name of calling function */
- const char*  what          /* I : name of allocated object */
+        size_t       nelem,
+        size_t       elsize,
+        int          doexit,
+        const char*  where,
+        const char*  what
 )
 {
-  void *result = calloc (nelem , elsize);
-  if ( result != NULL )
+    void *result = calloc(nelem, elsize);
+    if (result != NULL)
     {
-      return result;
+        return result;
     }
-  else
-  {
-    char* msg;
-    sprintf(msg, "Fatal: in %s, no memory for %s (%ld elements size %ld)\n",
-	    where, what, nelem, elsize);
-    if ( doexit )
-      error(msg);
-    else
-      return result ;
-  }
+    char msg[256];
+    snprintf(msg, sizeof(msg),
+             "Fatal: in %s, no memory for %s (%zu elements size %zu)\n",
+             where, what, nelem, elsize);
+    if (doexit)
+    {
+        fprintf(stderr, "%s", msg);
+        exit(EXIT_FAILURE);
+    }
+    return NULL;
 }
 /* ------------------------------------------------------------------- */
 
 /* ------------------------------------------------------------------- */
 void GenFree( void* ptr )
 {
-  if ( ptr != NULL )
-    free( ptr ) ;
+    if ( ptr != NULL )
+        free( ptr ) ;
 }
 /* ------------------------------------------------------------------- */
